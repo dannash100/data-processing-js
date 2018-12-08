9d9423b07cd4ba8badbad54e6d229db625e591cb
dannash100 <danster15@gmail.com>
2018-12-08 19:16:43 +1300

    first two exercises

---
+++ Chapter-3-master/read.js
@@ -0,0 +1,9 @@
+const file = require('./toolkit/file.js')
+
+file.read('./data/earthquakes.csv')
+  .then(textFileData => {
+    console.log(textFileData)
+  })
+  .catch(err => {
+    console.error('an error occurred \n' + err)
+  })
