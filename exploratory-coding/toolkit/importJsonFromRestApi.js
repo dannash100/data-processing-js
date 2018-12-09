3c3d17b89b5107408bbfd6644fb30f77eb5dedb0
dannash100 <danster15@gmail.com>
2018-12-09 19:30:55 +1300

    chapter 5 setup

---
+++ exploratory-coding/toolkit/importJsonFromRestApi.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const request = require('request-promise')
+
+function importJsonFromRestApi (url) {
+  return request.get({
+    uri: url,
+    json: true
+  })
+};
+
+module.exports = importJsonFromRestApi
